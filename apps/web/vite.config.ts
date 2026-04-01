d73c0b864289193a054252aaf85290add0f842bd
Ryan Whiteleather <RyanWhiteleather383@gmail.com>
2026-04-01 14:27:31 -0400

    Fix path

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig, searchForWorkspaceRoot } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+    base: command === "build" ? "/ray-tracer/" : "/",
     plugins: [react()],
     server: {
         fs: {
             allow: [searchForWorkspaceRoot(process.cwd()), "../../crates/wasm/pkg"],
         },
     },
-});
+}));
